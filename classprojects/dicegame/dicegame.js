d6b1b725c6373072f380911f3d3f35bcc902c6fe
bunkch01 <bunkch01@users.noreply.github.com>
2014-12-07 12:13:09 -0600

    Update dicegame.js

--- classprojects/dicegame/dicegame.js
+++ classprojects/dicegame/dicegame.js
@@ -14,11 +14,11 @@ od1res = document.getElementById(od1);
 od2res = document.getElementById(od2);
 od3res = document.getElementById(od3);
 yd1res.innerHTML = "yd1roll";
-yd2res.innerHTML = "yd2roll"";
+yd2res.innerHTML = "yd2roll";
 yd3res.innerHTML = "yd3roll";
-od1res.innerHTML = "od1roll"";
+od1res.innerHTML = "od1roll";
 od2res.innerHTML = "od2roll";
-od3res.innerHTML = "od3roll"";
+od3res.innerHTML = "od3roll";
 if (ydrolltot >= odrolltot) { document.getElementById(res).innerHTML = "You Win!"; }
 else { document.getElementById(res).innerHTML = "You Lose!"; }
 }
