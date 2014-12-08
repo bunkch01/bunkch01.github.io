02906326e5e1d8ded3f5520a538919142fa23d95
bunkch01 <bunkch01@users.noreply.github.com>
2014-12-08 15:17:00 -0600

    Update shop.js

--- classprojects/shopping/shop.js
+++ classprojects/shopping/shop.js
@@ -2,7 +2,7 @@ addItemFunc = function() {
 list = document.getElementById("groceryList");
 listLi = document.createElement("li");
 grocItem = document.getElementById("groceryItem").value;
-liContents = createTextNode(grocitem);
+liContents = createTextNode(grocItem);
 listLi.appendChild(liContents);
 list.appendChild(listLi);
 }
