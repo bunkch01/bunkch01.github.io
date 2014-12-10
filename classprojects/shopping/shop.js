dd93a4755e155c2fda9ae0b25289cdf6ec548b97
bunkch01 <bunkch01@users.noreply.github.com>
2014-12-10 15:08:23 -0600

    Update shop.js

--- classprojects/shopping/shop.js
+++ classprojects/shopping/shop.js
@@ -1,10 +1,12 @@
 addItemFunc = function() {
+itemClass = document.getElementById("itemType").value;  
 list = document.getElementById("groceryList");
 listLi = document.createElement("li");
 liCheck = document.createElement("input");
 liCheck.setAttribute("type", "checkbox");
 grocItem = document.getElementById("groceryItem").value;
 liContents = document.createTextNode(grocItem);
+listLi.classList.add(itemClass);
 listLi.appendChild(liContents);
 list.appendChild(liCheck);
 list.appendChild(listLi);
