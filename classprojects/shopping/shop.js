f8151d7625207306131c25f4f1eef56c7b247b7d
bunkch01 <bunkch01@users.noreply.github.com>
2014-12-10 15:33:02 -0600

    Update shop.js

--- classprojects/shopping/shop.js
+++ classprojects/shopping/shop.js
@@ -6,10 +6,16 @@ liCheck = document.createElement("input");
 liCheck.setAttribute("type", "checkbox");
 grocItem = document.getElementById("groceryItem").value;
 liContents = document.createTextNode(grocItem);
+liCheck.onclick = checkListFunc;
 listLi.classList.add(itemClass);
 listLi.appendChild(liCheck);
 listLi.appendChild(liContents);
 list.appendChild(listLi);
 resetBox = document.getElementById("groceryItem");
 resetBox.value = "";
+}
+
+checkListFunc = function() {
+  if (this.checked = true;) { this.parentNode.classList.add("done"); }
+  else { this.parentNode.classlist.remove("done"); }
 }
